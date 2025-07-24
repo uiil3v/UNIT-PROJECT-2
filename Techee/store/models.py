0ebb57cce2c25f2c226ea61460fe925e2b0f07ba
Saif <sknalyami@gmail.com>
2025-07-24 12:15:07 +0300

    Finished the products page with responsive design and backend

--- Techee/store/models.py
+++ Techee/store/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('phones', 'Phones'),
+        ('laptops', 'Laptops'),
+        ('headphones', 'Headphones'),
+        ('watches', 'Watches'),
+        ('tablets', 'Tablets'),
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    image = models.ImageField(upload_to='images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.price} SAR"
--- Techee/store/views.py
+++ Techee/store/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from .models import Product 
+from django.core.paginator import Paginator
 
-def products_view (request: HttpRequest):
-    return render(request, "store/products.html")
+def products_view(request: HttpRequest):
+    all_products = Product.objects.all()
+    paginator = Paginator(all_products, 8)  
+    page_number = request.GET.get("page") 
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "store/products.html", {"products": page_obj})
 
 def detail_view(request: HttpRequest, product_id: int):
     return render(request, "store/detail.html")

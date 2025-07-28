b55fe14a996703696d8283bda031bc57c05c10c2
Saif <sknalyami@gmail.com>
2025-07-29 01:44:10 +0300

    add a setting page to main app, and implement dark mode to base page

--- Techee/main/urls.py
+++ Techee/main/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.home_view, name="home_view"),
     path('contact/', views.contact_view, name="contact_view"),
     path('about/', views.about_view, name="about_view"),
+    path('settings/', views.settings_view, name="settings_view"),
+    
 ]
--- Techee/main/views.py
+++ Techee/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 
 
@@ -14,3 +14,13 @@ def contact_view (request: HttpRequest):
 # About View
 def about_view (request: HttpRequest):
     return render(request, "main/about.html")
+
+
+def settings_view(request: HttpRequest):
+    if request.method == "POST":
+        mode = request.POST.get("mode", "light")  
+        response = redirect(request.META.get("HTTP_REFERER", "/"))  
+        response.set_cookie("mode", mode, max_age=60 * 60 * 24 * 30)  
+        return response
+
+    return render(request, "main/settings.html")

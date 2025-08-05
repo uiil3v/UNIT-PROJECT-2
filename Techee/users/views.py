3dcfafb3efcde10d91d6cba58b647f2f5319a017
Saif <sknalyami@gmail.com>
2025-08-05 13:32:38 +0300

    Add tabs and fix dark mode styling

--- Techee/users/views.py
+++ Techee/users/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login
 from django.contrib import messages
 import re  
-from django.contrib.auth import logout
-
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 def register_view(request):
     if request.method == "POST":
